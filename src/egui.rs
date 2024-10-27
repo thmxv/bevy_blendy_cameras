15dbba38ecbd642e92e7ebc624031e5f736cd92c
Xavier Thomas <xavier.thomas.1980@gmail.com>
2024-10-27 17:47:26 -0300

    Fix panic when egui fails to return a context
    Fix originally make in bevy_panorbit_camera by:
      Mat Stevenson <mat@offaxis.dev>
      Plonq

--- src/egui.rs
+++ src/egui.rs
@@ -31,10 +31,13 @@ pub(crate) fn check_egui_wants_focus(
     // contexts want focus, then it must be the one that the user is
     // interacting with.
     let new_wants_focus = windows.iter().any(|window| {
-        let ctx = contexts.ctx_for_entity_mut(window);
-        ctx.wants_pointer_input()
-            || ctx.wants_keyboard_input()
-            || ctx.is_pointer_over_area()
+        if let Some(ctx) = contexts.try_ctx_for_entity_mut(window) {
+            ctx.wants_pointer_input()
+                || ctx.wants_keyboard_input()
+                || ctx.is_pointer_over_area()
+        } else {
+            false
+        }
     });
     let new_res = EguiWantsFocus {
         prev: wants_focus.curr,
